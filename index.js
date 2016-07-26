bbc9a1625e24a3fd0d5588a4d2dd379b8961ed32
Aaron Brewer <spaceribs@gmail.com>
2016-07-26 08:53:21 -0400

    Server Submodule and Setup (#29)
    Server Submodule and setup script

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var jsf = require('json-schema-faker');
-var prettyjson = require('prettyjson');
-var util = require('util');
-
-var models = require('./models');
-
-util.log('\n' + prettyjson.render(jsf(models.db.place, models.refs)));
